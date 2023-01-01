509dfd3e3d6f4ebba0fb8752bf2901a11838f478
Kushio Tatsukio <66260493+Tatsukio@users.noreply.github.com>
2023-01-01 22:43:05 +0500

    Добавьте файлы проекта.

---
+++ ParaWorldStatus/Model/Misc.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace ParaWorldStatus.Model
+{
+    public static class Misc
+    {
+        public static int ParseUint(this string txt)
+        {
+            return int.TryParse(txt, out var value) && value >= 0 ? value : 0;
+        }
+
+        /// <summary>
+        /// The Unix Epoch date (1970-1-1 00:00:00).
+        /// </summary>
+        public static DateTime UnixEpoch { get; } = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long GetUnixTimestampSeconds(this DateTime time)
+        {
+            var univDateTime = time.ToUniversalTime();
+            return (long)(univDateTime - UnixEpoch).TotalSeconds;
+        }
+
+        public static Stream OpenResourceStream(Assembly assembly, string resource)
+        {
+            var assemblyName = assembly.GetName().Name;
+            var stream = assembly.GetManifestResourceStream($"{assemblyName}.{resource}");
+            if (stream == null)
+            {
+                throw new FileNotFoundException($"{resource} was not found");
+            }
+            return stream;
+        }
+
+    }
+}
